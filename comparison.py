9df5480ce04ec5491bba4d6af36e3fd4533b9ab9
elsaaf1 <elsaaef@gmail.com>
2026-07-01 14:55:03 +0700

    Update comparison.py

--- comparison.py
+++ comparison.py
@@ -5,8 +5,8 @@ import numpy as np
 def show():
  st.title("Perbandingan Algoritma")
  alg=["K-Means","Agglomerative"]
- sil=[0.398,0.411]
- dbi=[0.905,0.869]
+ sil=[0.419,0.412]
+ dbi=[0.863,0.869]
  x=np.arange(2);w=0.35
  fig,ax=plt.subplots()
  b1=ax.bar(x-w/2,sil,w,label="Silhouette")
